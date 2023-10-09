ccc7f81b4601f640c55d0b590227bd0067fc6cbb
broyettore <broyettore@gmail.com>
2023-10-09 15:51:10 +0200

    apis created, exercise done without bonus

---
+++ la-mia-pizzeria-static/Controllers/API/PizzasController.cs
@@ -0,0 +1,117 @@
+﻿using Humanizer;
+using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace la_mia_pizzeria_static.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+
+        private readonly PizzaContext _myDb;
+
+        public PizzasController(PizzaContext myDb)
+        {
+            _myDb = myDb;
+        }
+
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+            List<Pizza> pizzas = _myDb.Pizzas.Include(p => p.Category)
+                .Include(p => p.Ingredients)
+                .ToList();
+
+            return Ok(pizzas);
+        }
+
+        [HttpGet]
+        public IActionResult GetPizzasByName(string? name)
+        {
+            if (name == null)
+            {
+                return BadRequest(new { Message = "No parameter inserted" });
+            }
+
+            List<Pizza> foundPizzas = _myDb.Pizzas.Where(p => p.Name.ToLower().Contains(name.ToLower())).ToList();
+
+            return Ok(foundPizzas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPizzaById(int id)
+        {
+            Pizza? foundPizza = _myDb.Pizzas.Where(p => p.PizzaId == id).Include(p => p.Category)
+                .Include(p => p.Ingredients).FirstOrDefault();
+
+            if (foundPizza != null)
+            {
+                return Ok(foundPizza);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Pizza newPizza)
+        {
+            try
+            {
+                _myDb.Pizzas.Add(newPizza);
+                _myDb.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Modify(int id, [FromBody] Pizza updatedPizza)
+        {
+            Pizza? pizzaToUpdate = _myDb.Pizzas.Where(p => p.PizzaId == id).FirstOrDefault();
+
+            if(pizzaToUpdate != null) 
+            {
+                pizzaToUpdate.Name = updatedPizza.Name;
+                pizzaToUpdate.ImgPath = updatedPizza.ImgPath;
+                pizzaToUpdate.Price = updatedPizza.Price;
+                pizzaToUpdate.Description = updatedPizza.Description;
+                pizzaToUpdate.CategoryId = updatedPizza.CategoryId;
+
+                _myDb.SaveChanges();
+
+                return Ok();
+            } else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Pizza? pizzaToDelete = _myDb.Pizzas.Where(p => p.PizzaId == id).FirstOrDefault();
+
+            if(pizzaToDelete == null) 
+            {
+                return NotFound();
+            } else
+            {
+                _myDb.Pizzas.Remove(pizzaToDelete);
+                _myDb.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
--- la-mia-pizzeria-static/Program.cs
+++ la-mia-pizzeria-static/Program.cs
@@ -1,6 +1,7 @@
 using la_mia_pizzeria_static.Database;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace la_mia_pizzeria_static
 {
@@ -18,6 +19,11 @@ namespace la_mia_pizzeria_static
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            // Codice di cofigurazione per il serializzatore JSON, in modo che ignori completamente le dipendenze cicliche di
+            // eventuali relazione N:N o 1:N presenti nel JSON risultante.
+            builder.Services.AddControllers().AddJsonOptions(x =>
+                            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 
             // Dependency injection
